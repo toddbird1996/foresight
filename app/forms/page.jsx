4ea83b1beec85496ea6a548af6900cfe1c0c9a1b
toddbird1996 <toddbird1996@gmail.com>
2026-02-22 09:38:11 -0600

    Update page.jsx

--- app/forms/page.jsx
+++ app/forms/page.jsx
@@ -1,30 +1,15 @@
 'use client';
-
-// app/forms/page.jsx
-import React, { useState, useEffect } from "react";
-import { getForms } from "../lib/forms";
+import { forms } from '../../lib/data';
 
 export default function FormsPage() {
-  const [forms, setForms] = useState([]);
-
-  useEffect(() => {
-    async function fetchForms() {
-      const data = await getForms();
-      setForms(data || []);
-    }
-    fetchForms();
-  }, []);
-
   return (
     <div>
       <h1>Available Court Forms</h1>
       <ul>
-        {forms.map((form) => (
+        {forms.map(form => (
           <li key={form.id}>
-            {form.form_name} - {form.jurisdiction}{" "}
-            <a href={form.form_url} target="_blank" rel="noopener noreferrer">
-              Download
-            </a>
+            {form.form_name} - {form.jurisdiction} 
+            <a href={form.form_url} target="_blank" rel="noopener noreferrer">Download</a>
           </li>
         ))}
       </ul>
