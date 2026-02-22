06ac8e6b817bacebe3f2d1c37ad43fa0d00d682c
toddbird1996 <toddbird1996@gmail.com>
2026-02-21 22:39:57 -0600

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+// app/page.jsx
+import React from "react";
+import LandingPage from './components/LandingPage'; // fixed path
+
+export default function Home() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
