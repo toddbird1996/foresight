5c07663f4fd3eeefccb286e0824be5ed66dc5198
toddbird1996 <toddbird1996@gmail.com>
2026-02-27 23:21:15 -0600

    Update page.js

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -2,7 +2,7 @@
 'use client';
 
 import { useState } from 'react';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from "../../../lib/supabaseClient";
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
