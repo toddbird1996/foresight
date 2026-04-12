9a6bcc5af781c8781aef0f1297d190ee9b7e0f37
toddbird1996 <toddbird1996@gmail.com>
2026-04-12 09:26:54 -0600

    Add Footer import to cases page

--- app/cases/page.jsx
+++ app/cases/page.jsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { supabase } from '../../lib/supabaseClient';
 import { useRouter } from 'next/navigation';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import PageTitle from '../components/PageTitle';
 
 export default function CasesPage() {
